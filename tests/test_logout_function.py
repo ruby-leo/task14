3b5b3c657c8e06e3476f8b13847117760bea0e40
Nithya <nithya@Nithyas-MacBook-Pro.local>
2026-05-16 03:05:17 +0530

    Added error message if assertion fails

--- tests/test_logout_function.py
+++ tests/test_logout_function.py
@@ -11,4 +11,4 @@ class TestLogoutFunction:
             dashboard_page.click_new_alert_close_button()
         dashboard_page.click_profile_click_icon()
         dashboard_page.click_logout()
-        assert "/login" in login_page.get_current_url()
+        assert "/login" in login_page.get_current_url(), "Logout is not successful"
